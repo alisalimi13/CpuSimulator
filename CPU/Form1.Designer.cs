9e9f3bc5682f6289229fab091e291cf42dcbffaf
Ali Salimi <alisalimi@live.com>
2020-06-01 19:19:41 +0430

    project initialization

---
+++ CPU/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace CPU
+{
+    partial class CPU
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CPU));
+            this.label1 = new System.Windows.Forms.Label();
+            this.PC = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.IR = new System.Windows.Forms.TextBox();
+            this.AC = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Next = new System.Windows.Forms.Button();
+            this.Memory = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Memory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Memory";
+            // 
+            // PC
+            // 
+            this.PC.Location = new System.Drawing.Point(397, 43);
+            this.PC.Name = "PC";
+            this.PC.Size = new System.Drawing.Size(100, 20);
+            this.PC.TabIndex = 2;
+            this.PC.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(394, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Registers";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(360, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "PC";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(360, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "IR";
+            // 
+            // IR
+            // 
+            this.IR.Location = new System.Drawing.Point(397, 81);
+            this.IR.Name = "IR";
+            this.IR.Size = new System.Drawing.Size(100, 20);
+            this.IR.TabIndex = 6;
+            // 
+            // AC
+            // 
+            this.AC.Location = new System.Drawing.Point(397, 119);
+            this.AC.Name = "AC";
+            this.AC.Size = new System.Drawing.Size(100, 20);
+            this.AC.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(360, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "AC";
+            // 
+            // Next
+            // 
+            this.Next.Location = new System.Drawing.Point(363, 239);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(134, 81);
+            this.Next.TabIndex = 9;
+            this.Next.Text = "Next";
+            this.Next.UseVisualStyleBackColor = true;
+            this.Next.Click += new System.EventHandler(this.Next_Click);
+            // 
+            // Memory
+            // 
+            this.Memory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Memory.Location = new System.Drawing.Point(16, 40);
+            this.Memory.Name = "Memory";
+            this.Memory.Size = new System.Drawing.Size(289, 280);
+            this.Memory.TabIndex = 10;
+            this.Memory.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Memory_CellContentClick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(549, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(221, 312);
+            this.label6.TabIndex = 11;
+            this.label6.Text = resources.GetString("label6.Text");
+            // 
+            // CPU
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 332);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Memory);
+            this.Controls.Add(this.Next);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.AC);
+            this.Controls.Add(this.IR);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PC);
+            this.Controls.Add(this.label1);
+            this.Name = "CPU";
+            this.Text = "CPU";
+            ((System.ComponentModel.ISupportInitialize)(this.Memory)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox PC;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox IR;
+        private System.Windows.Forms.TextBox AC;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Next;
+        private System.Windows.Forms.DataGridView Memory;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ CPU/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CPU
+{
+    public partial class CPU : Form
+    {
+        DataTable mem;
+        int pc, ir, ac, n, adr;
+        public CPU()
+        {
+            InitializeComponent();
+
+            // Creating Memory
+            mem = new DataTable();
+            mem.Columns.Add("Address");
+            mem.Columns.Add("Contain");
+            for (int i = 0; i < 1000; i++)
+                mem.Rows.Add();
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i < 10)
+                    mem.Rows[i][0] = "00" + i.ToString();
+                if (i >= 10 && i < 100)
+                    mem.Rows[i][0] = "0" + i.ToString();
+                if (i >= 100 && i < 1000)
+                    mem.Rows[i][0] = i.ToString();
+            }
+            for (int i = 0; i < 1000; i++)
+                mem.Rows[i][1] = "0000";
+
+            // A Program For CPU
+            mem.Rows[0][1] = "0014";
+            mem.Rows[1][1] = "2015";
+            mem.Rows[2][1] = "1016";
+            mem.Rows[3][1] = "3015";
+            mem.Rows[4][1] = "1017";
+            mem.Rows[5][1] = "4015";
+            mem.Rows[6][1] = "1018";
+            mem.Rows[7][1] = "5015";
+            mem.Rows[8][1] = "1019";
+            mem.Rows[9][1] = "6015";
+            mem.Rows[10][1] = "1020";
+            mem.Rows[11][1] = "7013";
+            mem.Rows[12][1] = "7014";
+            mem.Rows[13][1] = "8000";
+            mem.Rows[14][1] = "0003";
+            mem.Rows[15][1] = "0002";
+
+            Memory.DataSource = mem;
+
+            // Initial Value
+            PC.Text = "000";
+            AC.Text = "0000";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Next_Click(object sender, EventArgs e)
+        {
+            // Setting Variables and IR Register
+            pc = Convert.ToInt32(PC.Text);
+            IR.Text = mem.Rows[pc][1].ToString();
+            ir = Convert.ToInt32( IR.Text );
+            n = ir / 1000;
+            adr = ir - (n * 1000);
+            ac = Convert.ToInt32(AC.Text);
+
+            // Doing Instruction
+            switch (n)
+            {
+                case 0:
+                    ac = Convert.ToInt32(mem.Rows[adr][1]);
+                    break;
+
+                case 1:
+                    mem.Rows[adr][1] = AC.Text;
+                    break;
+
+                case 2:
+                    ac = Convert.ToInt32(AC.Text) + Convert.ToInt32(mem.Rows[adr][1]);
+                    break;
+
+                case 3:
+                    ac = Convert.ToInt32(AC.Text) - Convert.ToInt32(mem.Rows[adr][1]);
+                    break;
+
+                case 4:
+                    ac = Convert.ToInt32(AC.Text) * Convert.ToInt32(mem.Rows[adr][1]);
+                    break;
+
+                case 5:
+                    ac = Convert.ToInt32(AC.Text) / Convert.ToInt32(mem.Rows[adr][1]);
+                    break;
+
+                case 6:
+                    ac = Convert.ToInt32(AC.Text) % Convert.ToInt32(mem.Rows[adr][1]);
+                    break;
+
+                case 7:
+                    pc = adr - 1;
+                    break;
+
+                case 8:
+                    pc = pc - 1;
+                    break;
+            }
+
+            // Setting AC and PC Registers
+            if (ac >= 10000)
+            {
+                int i = ac / 10000;
+                ac = ac - (i * 10000);
+            }
+            if ( ac < 0 )
+                AC.Text = "0000";
+            else if (ac >= 0 && ac < 10)
+                AC.Text = "000" + ac.ToString();
+            else if (ac >= 10 && ac < 100)
+                AC.Text = "00" + ac.ToString();
+            else if (ac >= 100 && ac < 1000)
+                AC.Text = "0" + ac.ToString();
+            else if (ac >= 1000 && ac < 10000)
+                AC.Text = ac.ToString();
+
+            if ( pc < 0 && pc >= 999 )
+                PC.Text = "000";
+            else if (pc >= 0 && pc < 9)
+                PC.Text = "00" + (pc + 1).ToString();
+            else if (pc >= 9 && pc < 99)
+                PC.Text = "0" + (pc + 1).ToString();
+            else if (pc >= 99 && pc < 999)
+                PC.Text = (pc + 1).ToString();
+        }
+
+        private void Memory_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
